1bcba29609cd6df4a9ab2bf60b3cf41ae15798f8
Brandon Govindarajoo <bgovi@umich.edu>
2022-02-24 11:06:26 -0500

    Removed utils directory. Added functions to payload

---
+++ src/route_parser/index.js
@@ -0,0 +1,8 @@
+const id_check = require('./indentifier_check/identifier_check.js')
+
+
+module.exports = {
+    'valid_identifier': id_check.valid_identifier,
+    'check_identifier_error': id_check.check_identifier_error
+}
+
