2de14e5003b3b0495df2643a7ed432b9be475153
Brandon Govindarajoo <bgovi@umich.edu>
2022-03-09 11:02:07 -0500

    Modified payload.js and jwt_utils. Cleaned up src tree

---
+++ src/route_parser/jwt_util/index.js
@@ -0,0 +1,23 @@
+/*
+
+
+
+
+*/
+
+function jwt_is_valid(jwt_token, jwt_key) {
+
+}
+
+function jwt_is_expired(json_jwt) {
+
+}
+
+function jwt_to_json(jwt_token) {
+
+}
+
+module.exports = {
+    'jwt_is_valid': jwt_is_valid,
+    'jwt_to_json': jwt_to_json
+}
