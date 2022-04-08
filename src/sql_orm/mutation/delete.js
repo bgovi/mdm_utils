3a2640c6e02e270bf68220db95ebb4d7fb4a85c9
Brandon Govindarajoo <bgovi@umich.edu>
2022-04-08 10:14:27 -0400

    Completed test cases and implementation for delete operations

--- src/sql_orm/mutation/delete.js
+++ src/sql_orm/mutation/delete.js
@@ -15,15 +15,26 @@ set default set null
 
 payload (null as default)
 */
+const rp = require('../../route_parser')
+const rs = require('./return_str.js')
+
 
 function delete_statement(schema_name, table_name, row_id) {
+    rp.CheckIdentifierError(schema_name)
+    rp.CheckIdentifierError(table_name)
 
     return { "text": `DELETE FROM "${schema_name}"."${table_name}" WHERE id =$1`, "values": [row_id] }
 
 }
 
 function delete_at_statement(schema_name, table_name, row_id) {
+    rp.CheckIdentifierError(schema_name)
+    rp.CheckIdentifierError(table_name)
 
     return { "text": `UPDATE "${schema_name}"."${table_name}" set _deleted_at = current_timestamp WHERE id =$1`, "values": [row_id] }
+}
 
+module.exports = {
+    'delete_statement': delete_statement,
+    'delete_at_statement': delete_at_statement
 }
