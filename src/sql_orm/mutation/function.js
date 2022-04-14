39bf504637765a766b4871f648264716923ca347
Brandon Govindarajoo <bgovi@umich.edu>
2022-04-14 18:17:43 -0400

    Added function.js need to test

--- src/sql_orm/mutation/function.js
+++ src/sql_orm/mutation/function.js
@@ -1,10 +1,11 @@
 /*
-
+Creates function statement
 
 let function_params = {
     "user_id": "",
     "user_name": "",
     "row_data": ""
+    "default_object": ""
 }
 */
 const sutil = require('../../sutils')
@@ -17,22 +18,33 @@ function ExecuteStatement(schema_name, function_name, row_data,values, index, fu
     fn(row_data,user_id,user_name)
     */
     //if on conflict or on restraint
-    let i = index
-    let i2 = index+1
-    let i3 = index+1
+    let params = ParseFunctionParams(function_params)
+    let x = CreateFunctionParameters(row_data, params, values, index) 
     if (is_select) {
-        return { "query": `SELECT * FROM "${schema_name}"."${function_name}"($1)`, "params": [row_data] }
+        let qx = `SELECT * FROM "${schema_name}"."${function_name}"(${JSON.stringify(x.args[0])}, ${x.args[1]}, ${x.args[2]}, ${JSON.stringify(params.default_object)})`
+        return { 'text': qx , "values": values, 'new_index': x.new_index }
     } else {
-        return { "query": `SELECT "${schema_name}"."${function_name}"($1)`, "params": [row_data] }
+        let qx = `SELECT "${schema_name}"."${function_name}"(${JSON.stringify(x.args[0])}, ${x.args[1]}, ${x.args[2]}, ${JSON.stringify(params.default_object)})`
+        return { "query": qx, "values": values, 'new_index': x.new_index }
     }
-    //returning?
 }
 
 //
-function CreateFunctionParameters() {
+function CreateFunctionParameters(row_data, params, values, index) {
 
+    let args = []
+    let bparams = bindp.AddBindParameters('row_data', row_data, {}, values, index, params.bind_type, array_type = params.array_type)
+    index = bparams.new_index
+    args.push(`"${bparams.pholder}"` )
 
+    bparams = bindp.AddBindParameters('user_id', params.user_id, {}, values, index, params.bind_type, array_type = params.array_type)
+    index = bparams.new_index
+    args.push(`"${bparams.pholder}"` )
 
+    bparams = bindp.AddBindParameters('user_name', params.user_id, {}, values, index, params.bind_type, array_type = params.array_type)
+    index = bparams.new_index
+    args.push(`"${bparams.pholder}"` )
+    return {'args': args, 'new_index': index}
 }
 
 
@@ -55,6 +67,10 @@ function ParseFunctionParams(init_params) {
         if (sutil.IsBoolean(dparams['is_select'])) { function_params['is_select'] = dparams['is_select'] }
         else { function_params['is_select'] = true }
     } else { function_params['is_select'] = true }
+
+    if (dparams.hasOwnProperty('default_object')) { function_params['default_object'] = String(dparams['default_object']) } 
+    else {function_params['default_object'] = {}}
+
     return function_params
 }
 
